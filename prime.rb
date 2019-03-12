30840e5d0f9835bf98d718790342c3d2649d0115
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-12 17:20:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,13 @@
 # Add  code here!
 def prime?(int)
-  for i in 2...(int.abs)
-      if int < 1
-        return false
-      elsif (int % i) == 0
+  if int < 1
+    return false
+  elsif i > 1
+    for i in 2...(int)
+      if (int % i) == 0
         return false
       end
     end
-true
+    true
+
 end
